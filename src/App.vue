92412f619d2fea86d266771a2008e5507464714c
Bogdan Luca <luca.bogdan@gmail.com>
2018-04-11 15:12:18 +0300

    Added another transaction and created a list using v-for.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <div id="app" class="container">
     <h3 class="mb-4">My Finance Manager :: Transactions</h3>
     <TransactionHeader/>
-    <Transaction :transaction="transaction"/>
+    <Transaction
+      v-for="transaction in transactions"
+      :transaction="transaction"
+      :key="transaction.id"
+    />
   </div>
 </template>
 
@@ -18,12 +22,22 @@ export default {
   },
   data() {
     return {
-      transaction: {
-        date: Date.parse('2018-04-02'),
-        merchant: 'Asociatia Clujsters',
-        category: 'Education',
-        amount: 99.99,
-      },
+      transactions: [
+        {
+          id: 1,
+          date: Date.parse('2018-04-02'),
+          merchant: 'Asociatia Clujsters',
+          category: 'Education',
+          amount: 99.99,
+        },
+        {
+          id: 2,
+          date: Date.parse('2018-04-04'),
+          merchant: 'Casa Boema',
+          category: 'Food & Dining',
+          amount: 25.0,
+        },
+      ],
     };
   },
 };
